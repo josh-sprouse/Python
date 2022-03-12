5a2ce44fb47e7d2707fff2a0b1ea9eb40fc35630
Josh <mustachedemu@gmail.com>
2022-03-11 22:35:38 -0500

    Fourth Commit

--- spp/8.py
+++ spp/8.py
@@ -14,23 +14,23 @@ dsum = (getSum(n))
 if n != [2,5]:
     if n % 2 != 0:
         if dsum % 3 != 0:
-            if n != 5:
-                if int(repr(n) [-1]) !=5:
-                    # if n > 100
-                    #     n = (math.sqrt(n))
-
-                    # else (add n to list)
-
-
-                    print ("Prime?")
-
-                else:
-
-                    print ("Definitely not prime")
-
-            else:
-
-                print ("Prime")
+            # if n != 5:
+            #     if int(repr(n) [-1]) !=5:
+            #         # if n > 100
+            #         #     n = (math.sqrt(n))
+            #
+            #         # else (add n to list)
+            #
+            #
+            #         print ("Prime?")
+            #
+            #     else:
+            #
+            #         print ("Definitely not prime")
+            #
+            # else:
+            #
+            #     print ("Prime")
 
         else:
 
